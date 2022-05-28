b23d7a53d29fdc261942d8c3e732ff965eba1a74
Jager0002 <faisalmoin8991@gmail.com>
2022-05-28 23:49:30 +0600

    commit last

--- src/Pages/Home/BestSellingBike.js
+++ src/Pages/Home/BestSellingBike.js
@@ -4,21 +4,18 @@ const BestSellingBike = () => {
   return (
     <>
       <h2 className="text-3xl text-center mb-8 font-bold">Our Best Offer</h2>
-      <div className="w-4/5 grid lg:grid-cols-2 grid-cols-1 mx-auto justify-items-center">
+      <div className="w-4/5 grid lg:grid-cols-2 grid-cols-1 mx-auto justify-items-center items-center">
         <div className="">
-          <img
-            src={
-              "https://i.ibb.co/8rWwQ4T/honda-CRF-1000-L-Africa-Twin-2016-14-1-9175-medium.webp"
-            }
-            alt=""
-          />
+          <img src={"https://i.ibb.co/y6zkFS6/best-offer.webp"} alt="" />
         </div>
         <div className="mt-10 lg:p-20 p-12">
-          <h2 className="text-2xl ">BMW R1250GS</h2>
-          <h2 className="text-lg my-4 ">The Dream Bike you can ask for</h2>
+          <h2 className="text-2xl ">Get Your Dream Bike</h2>
+          <h2 className="text-lg my-2 ">
+            The Dream Bike you can ask for get custom modified parts.
+          </h2>
           <p>
-            We have parts of this bike and we can fully customly make you this
-            bike.
+            We have parts of this bike and we can fully customizable make you
+            this bike.
           </p>
         </div>
       </div>
